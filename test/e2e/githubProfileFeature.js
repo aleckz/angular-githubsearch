a498f17a9c424333759253a34e6f90e5cdd43204
Alex <aleksandernasr@gmail.com>
2015-07-30 14:39:31 +0100

    modified tests, stubbing github API, need to add access token, changed doSearch function in controller by removing dummy data and connecting to API

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,15 +1,33 @@
 describe('Github Profile finder', function(){
+
+  var searchBox = element(by.model('searchController.searchTerm'))
+  var searchButton = element(by.className('btn'))
+
+  beforeEach(function(){
+    browser.get('http://localhost:8080');
+  })
+
   it('has a title', function(){
-    browser.get('http://localhost:8080')
     expect(browser.getTitle()).toEqual('Github User Search')
   });
 
+  // it('finds profiles', function(){
+  //   searchBox.sendKeys('tansaku')
+  //   searchBtn.click();
+  //   expect(element(by.binding('user.login')).getText()).toEqual('tansaku');
+  // });
+
   it('finds profiles', function(){
-    browser.get('http://localhost:8080');
-
-    element(by.model('searchController.searchTerm')).sendKeys('spike01');
-    element(by.className('btn')).click();
-
-    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+    searchBox.sendKeys('tansaku');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchController.searchResult.items'));
+    expect(profiles.get(0).getText()).toEqual('tansaku')
   });
+
+  it('finds the last user', function(){
+    searchBox.sendKeys('tansaku');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchController.searchResult.items'));
+    expect(profiles.last().getText()).toEqual('stephenlloyd');
+  })
 });
